ac20a8503dcf3bd47066e8d9208fc5eb5126d999
AlexFlorea01 <alexandru-daniel.florea@student.tuiasi.ro>
2023-04-29 18:36:01 +0300

    configurare inregistrare

--- front-end-react/src/componente/Inregistrare-Conectare/Inregistrare.js
+++ front-end-react/src/componente/Inregistrare-Conectare/Inregistrare.js
@@ -1,10 +1,43 @@
 import './Inregistrare.css';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Meniu from '../Pagina principala/Meniu/Meniu';
 
 const Inregistrare = () => {
     
     let navigate = useNavigate();
+
+    const[dateIntrare, seteazaDateIntrare] = useState({
+        nume:'',
+        email:'',
+        parola:'',
+        repetaParola:''
+    })
+
+    const schimbaDateIntrare = (ceTastezNouEvent)=>{
+        seteazaDateIntrare((dateIntrareInitiale)=>{
+            let  dateIntrareNoi = {...dateIntrareInitiale};
+             dateIntrareNoi[ceTastezNouEvent.target.name] = ceTastezNouEvent.target.value; //.name in html diferit pentru fiecare field
+            return  dateIntrareNoi;
+        })
+    }
+    
+    useEffect(()=>{
+        console.log(dateIntrare)
+    },[dateIntrare])
+
+    const trimiteModifIntrari = (event) => {
+        event.preventDefault(); // previne reincarcarea browserului la submit buton
+        if(dateIntrare.name.length == 0 || dateIntrare.email.length == 0 || dateIntrare.password.length == 0)
+        {
+            console.log("Nu ai completat unul dintre campuri!")
+        }
+        else
+        {
+
+        }
+    }
+     
 
     return (
         <div className="signup-container">
@@ -21,22 +54,22 @@ const Inregistrare = () => {
                             <form className="sign-up-form">
                                 <label>
                                     <span>Nume</span>
-                                    <input type="text" name="name"/>
+                                    <input type="text" name="name" onChange={schimbaDateIntrare}/>
                                 </label>
                                 <label>
                                     <span>Email</span>
-                                    <input type="text" name="email"/>
+                                    <input type="text" name="email" onChange={schimbaDateIntrare}/>
                                 </label>
                                 <label>
                                     <span>Parola</span>
-                                    <input type="password" name="password"/>
+                                    <input type="password" name="password" onChange={schimbaDateIntrare}/>
                                 </label>
                                 <label>
                                     <span>Repeta parola</span>
-                                    <input type="password" name="doublePassword"/>
+                                    <input type="password" name="doublePassword" onChange={schimbaDateIntrare}/>
                                 </label>
                                 <label className="submit-btn-label">
-                                    <button>Inregistrare</button>
+                                    <button onClick={trimiteModifIntrari}>Inregistrare</button>
                                 </label>
                                 <label className="change-state-signin-signup">
                                     <p onClick={()=>{navigate('/conectare')}}>Ai deja un cont?Conecteaza-te.</p>
