282f49ff0cdb1d267b9b4f4894d76894a7d7fa7c
AlexFlorea01 <alexandru-daniel.florea@student.tuiasi.ro>
2023-05-15 00:12:05 +0300

    update back-end

---
+++ back-end-node/index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express();
+const PORT = 4000;
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+//Import routers
+// //const authRoute = require('./routes/auth'); 
+// const postRoute = require('./routes/posts');
+// const postNew = require('./routes/postNew');
+// const fetchProperty = require('./routes/fetchProperty');
+// const otherSection = require('./routes/otherSection');
+// const filterProps = require('./routes/filterProps');
+
+const cors = require('cors')
+app.use(cors())
+
+dotenv.config();
+
+//Connect to db
+// mongoose.connect(process.env.DB_CONNECT,
+//     {useNewUrlParser: true},
+//     ()=>{
+//     console.log("Connected to db!")
+// })
+
+let connect_to_mongose = async ()=>{
+    try {
+        let temp_obj_data = await mongoose.connect('mongodb://localhost:27017', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+
+        console.log('MongoDB connected!!');
+    } catch (err) {
+        console.log('Failed to connect to MongoDB', err);
+    }
+}
+connect_to_mongose();
+
+
+//MiddleWare
+app.use(express.json());
+
+
+
+//Route MiddleWares
+//anythin in authRoute will have '/api/user prefix => /api/user/register
+// app.use('/api/user',authRoute);
+// app.use('/api/posts',postRoute);
+// app.use('/api/newpost',postNew);
+// app.use('/api/fetchProperty', fetchProperty);
+// app.use('/api/otherSection',otherSection);
+// app.use('/api/filterProps',filterProps);
+
+//implementeaza aici
+app.get('/ceva',(req,res)=>
+{
+    res.send('ceva')
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening on port ${PORT}`)
+})
---
+++ back-end-node/modele/modelUtilizator.js
@@ -0,0 +1,26 @@
+//Mongoose este o bibliotecă pentru Node.js care permite dezvoltatorilor să definească scheme și să interacționeze cu baze de date MongoDB într-un mod mai simplu și mai intuitiv. MongoDB este un sistem de gestiune a bazelor de date NoSQL, care stochează datele sub forma de documente JSON, în loc să le stocheze în tabele și rânduri, așa cum se întâmplă în bazele de date relaționale.
+
+
+// importă biblioteca Mongoose și o atribuie la o constantă mongoose
+const mongoose = require('mongoose'); 
+
+
+//Se definește schema pentru utilizatori prin apelarea funcției mongoose.Schema(). 
+const userSchema = new mongoose.Schema({
+
+
+});
+
+
+// Se creează un model pentru colecția de utilizatori prin apelarea funcției mongoose.model(). Acesta primește două argumente: numele modelului și schema asociată. În acest caz, numele modelului este 'User', iar schema asociată este userSchema.
+module.exports = mongoose.model('User', userSchema);
+
+//În Mongoose, module.exports este utilizat pentru a exporta un model definit prin intermediul mongoose.model(). Modelul este apoi disponibil în alte module din aplicația Node.js prin intermediul require().
+
+//De exemplu, într-un alt fișier JavaScript, putem utiliza require() pentru a accesa modelul definit în fișierul anterior:
+
+// const User = require('./models/user');
+
+
+
+//Puteți utiliza metodele app.get(), app.post(), app.put() sau app.delete() pentru a defini rutele corespunzătoare pentru metodele HTTP GET, POST, PUT sau DELETE.
